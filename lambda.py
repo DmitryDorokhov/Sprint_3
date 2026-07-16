2c1ff47078c781f28700987ad72055f0c20baa33
DmitryDorokhov <dmitry.dorokhov.1709@yandex.ru>
2026-07-16 12:34:17 +0300

    lambda

---
+++ lambda.py
@@ -0,0 +1,9 @@
+people = ['Антон', 'Соня', 'Коля', 'Женя', 'Тоня', 'Стёпа']
+
+def say_to_all(func, sequence):
+
+for item in sequence:
+
+func(item)
+
+say_to_all(lambda a: print(f'Здравствуй, {a}!' if a[0] == 'С' else f'Привет, {a}!') ,people)
