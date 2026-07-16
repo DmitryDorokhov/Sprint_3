50cd6dde8c2047260620d8a6cd7ef0cdd59558a6
DmitryDorokhov <dmitry.dorokhov.1709@yandex.ru>
2026-07-16 12:36:49 +0300

    change lambda

--- lambda.py
+++ lambda.py
@@ -1,9 +1,7 @@
 people = ['Антон', 'Соня', 'Коля', 'Женя', 'Тоня', 'Стёпа']
 
 def say_to_all(func, sequence):
-
-for item in sequence:
-
-func(item)
+    for item in sequence:
+        func(item)
 
 say_to_all(lambda a: print(f'Здравствуй, {a}!' if a[0] == 'С' else f'Привет, {a}!') ,people)
